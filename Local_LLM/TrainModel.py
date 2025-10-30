3d02b0d741920adabf77894a8bd9539289a5ef96
cgamba533 <cgamba@calpoly.edu>
2025-10-30 16:10:23 +0100

    llm files

---
+++ Local_LLM/TrainModel.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# In terminal run: ollama create filename -f ./Modelfile
+
+df = pd.read_csv("data/")
+
+
+from llama3.2
+
+PARAMETER temperature 1
+
+SYSTEM """
+Instructions
+"""
